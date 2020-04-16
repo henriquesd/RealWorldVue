fd06bffbb4e37ba53838cd776dc65048ebfa92bf
henriquesd <hsd.foz@gmail.com>
2020-04-16 21:20:45 +0200

    Implemented configurations

---
+++ .prettierrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+    singleQuote: true, // this will convert double quotes to single quotes
+    semi: false // this will make sure that semicolons are not automatically inserted, and once that are there, will be removed;
+}
